158c48db0eefb19833841297bd7f4ca94a925373
van7ish <van7ish@hotmail.com>
2017-04-13 10:42:45 +0800

    添加深度任务详情页面

---
+++ KaiWan/DeepTaskDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DeepTaskDetailViewController.h
+//  KaiWan
+//
+//  Created by van7ish on 2017/4/12.
+//  Copyright © 2017年 chenguang. All rights reserved.
+//
+
+#import "MainCVViewController.h"
+
+@interface DeepTaskDetailViewController : MainCVViewController
+
+@end
